8932f8aa1fb0ebf67935728f177651a81bb3f57d
Danyalfaro <daniel.alfaro@upr.edu>
2023-04-03 16:23:08 -0400

    Added icon package.

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,22 +1,10 @@
 import { useEffect, useState } from "react";
-import { encodeChallengeToken } from "@/shared/util";
-import { ChallengePayload, TopArtistResponse } from "@/shared/models";
-import spotify from "@/services/spotify";
+import { FaWhatsapp } from "react-icons/fa";
 
 export const SocialLinks = () => {
   const [challengeToken, setChallengeToken] = useState("");
 
   useEffect(() => {}, []);
-
-  // const spotifyResponseToChallengePayload = (
-  //   response: TopArtistResponse
-  // ): ChallengePayload => {
-  //   const challengePayload: ChallengePayload = {
-  //     challenger: `${user.display_name}`,
-  //     topArtists: response.map((artist) => artist.id),
-  //   };
-  //   return challengePayload;
-  // };
 
   const challengeTokenToShareableLink = (challengeToken: string): string => {
     return `${process.env.NEXT_PUBLIC_CHALLENGE_URI}?challengeToken=${challengeToken}`;
--- src/pages/game.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from "react";
-import styles from "@/styles/Home.module.css";
-
-export const Game = () => {
-  return <main className={styles.main}></main>;
-};
-
-export default Game;
