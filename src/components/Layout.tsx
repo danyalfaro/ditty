f68b801c00908a8b97fdbcc3d6557925c55fb9ba
Danyalfaro <daniel.alfaro@upr.edu>
2023-04-06 12:37:38 -0400

    Removed unneeded CCS module.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,8 @@
-import styles from "@/styles/Home.module.css";
-
 export default function Layout({ children }: any) {
   return (
     <>
       <div className="flex justify-center items-start">
-        <main className={styles.main}>{children}</main>
+        <main className="main">{children}</main>
       </div>
     </>
   );
