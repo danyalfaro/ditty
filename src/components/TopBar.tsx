d1acb2e4e1da00d4e73f398e72fe23aff8ccbdee
Danyalfaro <daniel.alfaro@upr.edu>
2023-04-19 17:37:55 -0400

    Improved topbar styling.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,13 +1,13 @@
-import { FaHamburger, FaUser } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function TopBar() {
   return (
     <>
-      <div className="flex justify-center items-center h-12 w-full">
-        <div className="flex justify-between items-center h-full w-full px-8">
+      <div className="flex justify-center items-center w-full">
+        <div className="flex justify-between items-center h-full w-full px-8 py-4">
           <GiHamburgerMenu size={"24px"} />
-          <h1>DITTY</h1>
+          <h1 className="text-4xl">DITTY</h1>
           <FaUser size={"24px"} />
         </div>
       </div>
