b73479d4111a9c5c8f9cd22950de92dfb0d201b3
Danyalfaro <daniel.alfaro@upr.edu>
2023-09-28 12:18:08 -0400

    Added icons, added toggle to landing.

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -13,7 +13,7 @@ const Toggle = ({
         checked={checked}
         onChange={onChange}
       ></input>
-      <span className="peer-checked:bg-black peer-checked:before:translate-x-7 absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-slate-200 rounded-full before:duration-300 before:rounded-full before:absolute before:h-7 before:w-7 before:left-1 before:bottom-1 before:bg-white"></span>
+      <span className="peer-checked:bg-slate-900 peer-checked:before:translate-x-7 absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-slate-200 rounded-full before:duration-300 before:rounded-full before:absolute before:h-7 before:w-7 before:left-1 before:bottom-1 before:flex before:justify-center before:items-center before:pt-1 before:bg-white before:content-lightTheme dark:before:content-darkTheme"></span>
     </label>
   );
 };
